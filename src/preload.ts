64d15d2ae121a1e590cc72bd65758384a2a758a2
zen_.ith <124890239+hyZenith@users.noreply.github.com>
2025-12-01 00:10:17 +0530

    feat: electron APIs for different functions

--- src/preload.ts
+++ src/preload.ts
@@ -3,23 +3,24 @@
 
 import { contextBridge, ipcRenderer } from "electron";
 
+contextBridge.exposeInMainWorld("electronAPI", {
+  minimize: () => ipcRenderer.send("window:minimize"),
+  maximize: () => ipcRenderer.send("window:maximize"),
+  close: () => ipcRenderer.send("window:close"),
 
-contextBridge.exposeInMainWorld('electronAPI', {
-
-    selectFolder: async () => ipcRenderer.invoke('dialog:selectFolder'),
-    readFolder: async (path: string) => ipcRenderer.invoke('fs:readFolder', path),
-    readFile: (path: string) => ipcRenderer.invoke("fs:readFile", path),
-    writeFile: (path: string, content: string) =>
+  selectFolder: async () => ipcRenderer.invoke("dialog:selectFolder"),
+  readFolder: async (path: string) => ipcRenderer.invoke("fs:readFolder", path),
+  readFile: (path: string) => ipcRenderer.invoke("fs:readFile", path),
+  writeFile: (path: string, content: string) =>
     ipcRenderer.invoke("fs:writeFile", path, content),
-    renameFile: (oldPath: string, newPath: string) =>
-    ipcRenderer.invoke('fs:rename', oldPath, newPath),
-    deleteFile: (path: string) =>
-    ipcRenderer.invoke('fs:delete', path),
+  renameFile: (oldPath: string, newPath: string) =>
+    ipcRenderer.invoke("fs:rename", oldPath, newPath),
+  deleteFile: (path: string) => ipcRenderer.invoke("fs:delete", path),
 
-    createMarkdown: (
-      dirPath: string,
-      baseName?: string,
-      initialContent?: string
-    ) => ipcRenderer.invoke('fs:createMarkdown', dirPath, baseName, initialContent),
-
+  createMarkdown: (
+    dirPath: string,
+    baseName?: string,
+    initialContent?: string
+  ) =>
+    ipcRenderer.invoke("fs:createMarkdown", dirPath, baseName, initialContent),
 });
